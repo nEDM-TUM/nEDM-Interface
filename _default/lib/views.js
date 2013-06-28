421951753f9451358fb0bf1e6808fc55d0960fc7
Michael Marino <mmarino@gmail.com>
2013-06-28 10:17:58 +0200

    Add default design document.
    This provides a default design document for all databases for the nEDM experiment.

---
+++ _default/lib/views.js
@@ -0,0 +1,21 @@
+exports.conflicts = {
+  map: function(doc) {
+     if(doc._conflicts) {
+       // First emit the "winning" revision.
+       emit_rev(doc._rev)
+     
+       // Next emit all the revisions properly recorded as a conflict.
+       for(var a = 0; a < doc._conflicts.length; a++)
+         emit_rev(doc._conflicts[a])
+     }
+     
+     function emit_rev(rev) {
+       // Emit a value that will pull the correct revision for ?include_docs=true.
+       var key = [doc._id, rev]
+         , value = { "_id": doc._id, "_rev": rev };
+     
+       emit(key, value);
+     }
+  },
+  reduce : "_count"
+}
