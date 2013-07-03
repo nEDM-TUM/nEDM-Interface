b6c81e430c6d7b512aad6255921f05f31d39e949
Michael Marino <mmarino@gmail.com>
2013-07-03 23:35:17 +0200

    Removing scripts in lieu of a single js.

---
+++ head/scripts/nedm-base.js
@@ -0,0 +1,80 @@
+var session = require("session");
+
+var nedm = nedm || {};
+nedm.logged_in_as = null;
+
+$(document).bind('pageinit', function(event, ui) {
+  nedm.update_header();
+});
+
+session.on('change', function(userCtx) {
+  nedm.set_user_name(userCtx);
+  nedm.update_header();
+});
+
+
+
+nedm.namespace = function(namespaceString) {
+        var parts = namespaceString.split('.'),
+            parent = window,
+            currentPart = '';    
+            
+        for(var i = 0, length = parts.length; i < length; i++) {
+            currentPart = parts[i];
+            parent[currentPart] = parent[currentPart] || {};
+            parent = parent[currentPart];
+        }
+        return parent;
+}
+
+
+nedm.update_buttons = function() {
+        var user_status = nedm.check_user_status();
+        var loginbtn = $("a[id*=loginbtn]"); 
+        var logoutbtn = $("a[id*=logoutbtn]"); 
+        if (user_status == null) {
+            loginbtn.show();
+            logoutbtn.hide();
+        } else {
+            logoutbtn.find(".ui-btn-text").text("Logout (" + user_status + ")");
+            logoutbtn.show();
+            loginbtn.hide();
+        }
+
+}
+
+nedm.update_header = function() {
+  var hc = $('.headerChild');
+  if (hc.find("[data-role=header]").length == 0) {
+    hc.load("header.html", nedm.update_buttons); 
+  } else {
+    nedm.update_buttons();
+  }
+  
+}
+
+nedm.set_user_name = function(userCtx) {
+       nedm.logged_in_as = userCtx.name; 
+} 
+
+nedm.check_user_status = function() {
+    session.info(function(err, info) {
+       nedm.set_user_name(info.userCtx); 
+    });
+    return nedm.logged_in_as;
+}
+
+nedm.logout = function()
+{
+    session.logout();
+}
+
+nedm.validate = function(un, pw, callback)
+{
+    session.login(un, pw, 
+            function(err, response) {
+                var success = true;
+                if(err) success = false;
+                if (callback) callback(success); 
+            });
+}
