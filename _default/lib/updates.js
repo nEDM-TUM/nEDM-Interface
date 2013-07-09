9c1907f37cf25f09755ad6224e53fb5bcef0a83b
Michael Marino <mmarino@gmail.com>
2013-07-09 18:39:34 +0200

    Update update_with_timestamp.
    This now correctly builds a new document if the document is submitted as new.

--- _default/lib/updates.js
+++ _default/lib/updates.js
@@ -1,10 +1,32 @@
 
 exports.insert_with_timestamp = function(doc, req) {
     
+    // If it doesn't exist, we are making a new one
+    if (!doc) doc = {};
+
+    // For documents without uuids
+    if (!doc._id) doc._id = req.uuid;
+
+    // This would be odd, there should always be a body
+    if (!req.body) {
+        return [null, "No document to insert"];
+    }
+
+    // Pull the information from the body, insert into current document
+    _ref = JSON.parse(req.body); 
+    for (k in _ref) {
+        v = _ref[k];
+        doc[k] = v;
+    }
+
+    log(doc);
+
     var ts  = new Date().toUTCString();
-    var user = req.userCtx;
+    var user = req.userCtx.name;
     var message = 'insert with user (time) : ' + user +
                   ' (' + ts + ')'; 
+
+    // Always update the created_by and timestamp
     doc.created_by = user;
     doc.timestamp  = ts;
     return [doc, message]; 
