c7186b45436c9d1ff8850b7ad5bc033e6c3a889e
Michael Marino <mmarino@gmail.com>
2014-05-09 09:06:44 +0200

    Remove _default, prefer to insert nedm_default as data.

--- _default/lib/app.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  views: require('./views'),
-  updates: require('./updates'),
-  filters: require('./filters'),
-  rewrites: require('./rewrites'),
-  read_only: require('./access'),
-  validate_doc_update: require('./validate')
-};
