0b36b0595fe796528fbe7f41497c1fe6c75413ea
Michael Marino <mmarino@gmail.com>
2015-04-29 17:32:18 +0200

    Use js-cookie, instead of query-cookie.

--- head/scripts/nedm-base.js
+++ head/scripts/nedm-base.js
@@ -1,7 +1,5 @@
 // Global variables, to be exported and used by user code
 $ = require("jquery");
-require("jquery-cookie");
-$.cookie.json = true;
 var ace = require("ace");
 ace.config.set("basePath", "/nedm_head/_design/nedm_head/ace/");
 var jqm_cal = require("jqm-calendar");
