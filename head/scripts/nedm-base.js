c0eea7e332aff1e12cdb9392486afefe1d55b5a4
Michael Marino <mmarino@gmail.com>
2015-05-02 21:59:29 +0200

    Remove jqm_cal and nedm from "global" namespace.

--- head/scripts/nedm-base.js
+++ head/scripts/nedm-base.js
@@ -2,5 +2,5 @@
 $ = require("jquery");
 var ace = require("ace");
 ace.config.set("basePath", "/nedm_head/_design/nedm_head/ace/");
-var jqm_cal = require("jqm-calendar");
-var nedm = require("lib/nedm"); 
+require("jqm-calendar");
+require("lib/nedm");
